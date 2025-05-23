ffe46210e4f7429a6c990ff8b521c2db99f3761b
Krnsand <krnsandin@gmail.com>
2025-05-23 15:15:17 +0200

    Fix usernames in side panel

--- client/src/components/side-panel-users.tsx
+++ client/src/components/side-panel-users.tsx
@@ -1,14 +1,26 @@
+// components/SidePanelUsers.tsx
 import { useUsers } from "../hooks/use-users";
 
 export default function SidePanelUsers() {
   const users = useUsers();
 
   return (
-    <div className="flex flex-col p-4 bg-white shadow m-6 rounded-lg w-100">
+    <div style={{ width: "200px", background: "#f5f5f5", padding: "1rem" }}>
       <h3>Aktiva användare:</h3>
       <ul>
         {users.map((u, i) => (
-          <li key={i}>{u.name}</li>
+          <li key={i}>
+            {/* Visa namn och ev. avatar */}
+            {u.avatar && (
+              <img
+                src={u.avatar}
+                alt="avatar"
+                width={20}
+                style={{ display: "inline", marginRight: 8 }}
+              />
+            )}
+            {u.name}
+          </li>
         ))}
       </ul>
     </div>
